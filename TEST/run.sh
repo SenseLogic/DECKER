866a03682a69d68b31c1bc954bc6761bcc8d4268
senselogic <ecstatic.coder@gmail.com>
2019-01-28 19:59:34 +0100

    % spanish core test

--- TEST/run.sh
+++ TEST/run.sh
@@ -4,9 +4,13 @@ set -x
 ../decker --media_folder "SPANISH_CORE/" --dump_folder "SPANISH_CORE/" --verbose "spanish_core.apkg"
 read key
 
-../decker --front_title "Core" --back_title "Núcleo" --front_language "english" --back_language "spanish" --input_format "{{back_word}}[sound:{{back_sound}}]§<img src='{{front_image}}'><br><br>{{front_word}}" --input_format "<img src='{{front_image}}'><br><br>{{front_word}}§{{back_word}}[sound:{{back_sound}}]" --media_folder "SPANISH_CORE/" --dump_folder "SPANISH_CORE/" --trim --verbose "spanish_core.apkg" "SPANISH_CORE/spanish_core.lxf"
+../decker --input_format "{{back_word}}[sound:{{back_sound}}]§<img src='{{front_image}}'><br><br>{{front_word}}" --input_format "<img src='{{front_image}}'><br><br>{{front_word}}§{{back_word}}[sound:{{back_sound}}]" --media_folder "SPANISH_CORE/" --dump_folder "SPANISH_CORE/" --trim --verbose "spanish_core.apkg"
+read key
+
+../decker --front_title "Core" --back_title "Núcleo" --front_language "english" --back_language "spanish" --input_format "<img src='{{front_image}}'><br><br>{{front_word}}§{{back_word}}[sound:{{back_sound}}]" --media_folder "SPANISH_CORE/" --dump_folder "SPANISH_CORE/" --trim --verbose "spanish_core.apkg" "SPANISH_CORE/spanish_core.lxf"
 hexdump -C SPANISH_CORE/spanish_core.lxf > SPANISH_CORE/dump_lexilize_hexa.txt
 cat SPANISH_CORE/spanish_core.lxf | ~/data/WORK/TOOL/PROTOC/bin/protoc --decode_raw >SPANISH_CORE/dump_lexilize_proto.txt
+read key
 
 ../decker --front_title "Animals" --back_title "Animales" --front_language "english" --back_language "spanish" --input_format "{{front_word}}|{{back_word}}|{{front_transcription}}|{{back_transcription}}|{{front_sample}}|{{back_sample}}|{{front_comment}}|{{back_comment}}|{{front_gender}}|{{back_gender}}|{{image}}" --media_folder "SPANISH_ANIMALS/" --dump_folder "SPANISH_ANIMALS/" --verbose "spanish_animals.csv" "SPANISH_ANIMALS/spanish_animals.lxf"
 hexdump -C SPANISH_ANIMALS/spanish_animals.lxf > SPANISH_ANIMALS/dump_lexilize_hexa.txt
